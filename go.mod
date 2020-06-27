a141df3e7ea8e6b9737d76a83526afbd248ef8df
tomaslingotti <tomaslingotti@gmail.com>
2020-06-27 00:08:55 -0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/tomiok/data-manipulation-go
+
+go 1.13
+
+require (
+	golang.org/x/exp v0.0.0-20200513190911-00229845015e // indirect
+	gonum.org/v1/gonum v0.7.0
+)
---
+++ video-1/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"gonum.org/v1/gonum/blas/blas64"
+	"gonum.org/v1/gonum/mat"
+)
+
+func main() {
+	matrix()
+}
+
+func vectors() {
+	//Create 2 vectors
+	vectorA := mat.NewVecDense(3, []float64{1, 2, 3})
+	vectorB := mat.NewVecDense(3, []float64{4, 5, 6})
+	// dot product
+	dotProduct := mat.Dot(vectorA, vectorB)
+	fmt.Println("the dot product is", dotProduct)
+
+	//norm
+	//norm is the square root of the sum of all the values at power2
+	//for vector A is sqrt(1^2 + 2^2 + 3^2) = sqrt(14) = 3.74
+	norm := blas64.Nrm2(vectorA.RawVector())
+	fmt.Println("the norm is", norm)
+}
+
+func matrix() {
+	// matrix
+	matA := mat.NewDense(2, 2, []float64{2, 4, 5, 6})
+	matB := mat.NewDense(2, 2, []float64{4, 5, 9, 10})
+
+	matAA := mat.NewDense(2, 2, []float64{2, 4, 5, 6})
+	matBB := mat.NewDense(2, 2, []float64{4, 5, 9, 10})
+
+	//sum
+	matA.Add(matA, matB)
+	formatter := mat.Formatted(matA, mat.Prefix(""))
+	fmt.Printf("%v\n", formatter)
+
+	matBB.Add(matBB, matAA)
+	formatter_b := mat.Formatted(matBB, mat.Prefix(""))
+	fmt.Printf("%v\n", formatter_b)
+}
